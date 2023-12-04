b42db1185f2d5b122dccce08d203d19996649a4a
sorisi <124475244+sorisi@users.noreply.github.com>
2023-12-03 20:25:07 -0500

    Update stock.py
    correction

--- pages/stock.py
+++ pages/stock.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#import yfinance as yf
+import yfinance as yf
 import pandas as pd
 import numpy as np
 import datetime
@@ -24,7 +24,7 @@ def relativeret(df):
     return cumret
 
 if len(dropdown) > 0:
-    #df = yf.download(dropdown,start,end)['Adj Close']
+    df = yf.download(dropdown,start,end)['Adj Close']
     df = relativeret(yf.download(dropdown,start,end)['Adj Close'])
     st.header('*Returns of {}*'.format(dropdown))
     st.line_chart(df)
